d5f1f9fb5454e7695402e5867140df0f8d5556bf
mulsicpp <christianmulser000@gmail.com>
2024-01-17 19:14:28 +0100

    Reworked coordinates in HexField and added getSequence() and getArea() functions

---
+++ Assets/Scripts/BoardPiece.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardPiece : MonoBehaviour
+{
+    private int x, y;
+
+    public enum Type
+    {
+
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/HexField.cs
+++ Assets/Scripts/HexField.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static HexField;
 
 public class HexField : MonoBehaviour
 {
@@ -9,81 +11,105 @@ public class HexField : MonoBehaviour
     public GameObject cellPrefab;
 
     private GameObject[][] cells;
+
+    public struct Coord
+    {
+        public int x, y;
+
+        public Coord(int x, int y = 0, int z = 0)
+        {
+            this.x = x + z;
+            this.y = y + z;
+        }
+
+        public static Coord operator +(Coord c1, Coord c2)
+        {
+            return new(c1.x + c2.x, c1.y + c2.y);
+        }
+
+        public static Coord operator -(Coord c1, Coord c2)
+        {
+            return new(c1.x - c2.x, c1.y - c2.y);
+        }
+    }
+
+    private static readonly Coord[] CIRCLE_DELTAS = {
+            new(0, 0, -1),
+            new(-1),
+            new(0, 1),
+            new(0, 0, 1),
+            new(1),
+            new(0, -1)
+        };
+
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 deltaX = new Vector3 (1.0f, 0, 0);
+        Vector3 deltaX = new Vector3(1.0f, 0, 0);
         Vector3 deltaY = new Vector3(Mathf.Cos(2 * Mathf.PI / 3), 0, Mathf.Sin(2 * Mathf.PI / 3));
         Vector3 deltaZ = deltaX + deltaY;
 
         Debug.Log(deltaY);
 
+        Coord c = new(1);
+
         cells = new GameObject[radius * 2 + 1][];
-        for(int i = 0; i < cells.Length; i++)
+        for (int i = 0; i < cells.Length; i++)
         {
             cells[i] = new GameObject[radius * 2 + 1];
         }
-        
 
-        int x = 0, y = 0, z = 0;
-        set(x, y, Instantiate(cellPrefab, transform));
-        get(x, y).transform.position = x * deltaX + y * deltaY;
 
-        for(int i = 1; i <= radius; i++)
+        Coord coord = new Coord(0);
+
+        set(coord, Instantiate(cellPrefab, transform));
+        at(coord).transform.position = coord.x * deltaX + coord.y * deltaY;
+
+        for (int i = 1; i <= radius; i++)
         {
-            x++;
-            for(int j = 0; j < i; j++)
-            {
-                set(x, y, z, Instantiate(cellPrefab, transform));
-                get(x, y, z).transform.position = x * deltaX + y * deltaY + z * deltaZ;
-                z--;
-            }
+            coord.x++;
 
-            for (int j = 0; j < i; j++)
+            for (int k = 0; k < CIRCLE_DELTAS.Length; k++)
             {
-                set(x, y, z, Instantiate(cellPrefab, transform));
-                get(x, y, z).transform.position = x * deltaX + y * deltaY + z * deltaZ;
-                x--;
-            }
+                for (int j = 0; j < i; j++)
+                {
+                    set(coord, Instantiate(cellPrefab, transform));
+                    at(coord).transform.position = coord.x * deltaX + coord.y * deltaY;
+                    coord += CIRCLE_DELTAS[k];
+                }
 
-            for (int j = 0; j < i; j++)
-            {
-                set(x, y, z, Instantiate(cellPrefab, transform));
-                get(x, y, z).transform.position = x * deltaX + y * deltaY + z * deltaZ;
-                y++;
             }
+        }
 
-            for (int j = 0; j < i; j++)
-            {
-                set(x, y, z, Instantiate(cellPrefab, transform));
-                get(x, y, z).transform.position = x * deltaX + y * deltaY + z * deltaZ;
-                z++;
-            }
+        GameObject[] s;
 
-            for (int j = 0; j < i; j++)
-            {
-                set(x, y, z, Instantiate(cellPrefab, transform));
-                get(x, y, z).transform.position = x * deltaX + y * deltaY + z * deltaZ;
-                x++;
-            }
+        s = getSequence(new(radius - 1), new(0, 0, -1));
+        foreach(GameObject obj in s)
+        {
+            obj.transform.position = obj.transform.position + Vector3.up * 0.5f;
+        }
 
-            for (int j = 0; j < i; j++)
-            {
-                set(x, y, z, Instantiate(cellPrefab, transform));
-                get(x, y, z).transform.position = x * deltaX + y * deltaY + z * deltaZ;
-                y--;
-            }
+        s = getArea(new(0, 0), 2);
+        foreach (GameObject obj in s)
+        {
+            obj.transform.position = obj.transform.position + Vector3.down * 0.5f;
         }
     }
 
-    public GameObject get(int x, int y, int z = 0)
+    public GameObject at(int x, int y = 0, int z = 0)
     {
-        return cells[x + z + radius][y + z + radius];
+        return at(new Coord(x, y, z));
+
     }
 
-    public void set(int x, int y, GameObject cell)
+    public GameObject at(Coord coord)
     {
-        cells[x + radius][y + radius] = cell;
+        return cells[coord.x + radius][coord.y + radius];
+    }
+
+    public void set(Coord coord, GameObject cell)
+    {
+        cells[coord.x + radius][coord.y + radius] = cell;
     }
 
     public void set(int x, int y, int z, GameObject cell)
@@ -91,9 +117,58 @@ public class HexField : MonoBehaviour
         cells[x + z + radius][y + z + radius] = cell;
     }
 
+    public Cell cellAt(int x, int y, int z = 0)
+    {
+        return at(x, y, z).GetComponent<Cell>();
+    }
+
+    public Cell cellAt(Coord coord)
+    {
+        return at(coord).GetComponent<Cell>();
+    }
+
+    public GameObject[] getSequence(Coord start, Coord delta)
+    {
+        List<GameObject> sequence = new List<GameObject>();
+        while(isValidCoord(start))
+        {
+            sequence.Add(at(start));
+            start += delta;
+        }
+
+        return sequence.ToArray();
+    }
+
+    public GameObject[] getArea(Coord center, int radius)
+    {
+        List<GameObject> sequence = new List<GameObject>();
+
+        if (isValidCoord(center))
+            sequence.Add(at(center));
+        for (int i = 1; i <= radius; i++)
+        {
+            center.x++;
+            for (int k = 0; k < CIRCLE_DELTAS.Length; k++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (isValidCoord(center))
+                        sequence.Add(at(center));
+                    center += CIRCLE_DELTAS[k];
+                }
+            }
+        }
+        return sequence.ToArray();
+    }
+
+    private bool isValidCoord(Coord coord)
+    {
+        return Math.Abs(coord.x) <= radius && Math.Abs(coord.y) <= radius && Math.Abs(coord.x - coord.y) <= radius;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
